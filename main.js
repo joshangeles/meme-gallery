4959d4bcd44bfba225e191d9d3ff561d7ef614e6
Joshua Angeles <mintoesverm@yahoo.com>
2022-10-01 19:29:50 +0000

    completed meme gallery project

---
+++ main.js
@@ -0,0 +1,26 @@
+var $imageUrlInput = document.querySelector('input');
+var $addImageButton = document.querySelector('button');
+var $gallery = document.querySelector('main');
+var imageUrls = [
+  'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+  'https://c.tenor.com/uYP_Nkq8VPsAAAAd/coding-hello-world.gif',
+  'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg'
+];
+$addImageButton.addEventListener('click', function () {
+  if ($imageUrlInput.value.length > 0) {
+    imageUrls.push($imageUrlInput.value);
+    $imageUrlInput.value = '';
+    updateGallery();
+  }
+});
+function updateGallery() {
+  $gallery.innerHTML = '';
+  for (let i = 0; i <= (imageUrls.length - 1); i++) {
+    var $imageElement = document.createElement('img');
+    $imageElement.className = 'gallery-image';
+    $imageElement.src = imageUrls[i];
+    $gallery.appendChild($imageElement);
+  }
+}
+
+updateGallery();
